08e80a0da66e8c42c9a01a1331c76e816d0fe92f
Elina Satski <elina.satski@tptlive.ee>
2026-09-02 13:21:22 +0300

    add variable example

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello, World!";
+echo "Hello, World!"; // Writes out "Hello, World!" to the browser
+// hello
+//world
+
+/**
+ *  
+ * long comment
+ * some cool stuff
+ */
+
+$name = "John Doe"; // Variable to store the name
+$age = 30; // Variable to store the age
+echo $age;
+$age = $age * 2 *3 *4 *5 * 6 * 7 * 8 * 9 * 10; // Update the age variable by multiplying it by 2, 3, 4, 5, 6, 7, 8, 9, and 10
+echo $age;
+
+echo 2 + 2; // Outputs the result of 2 + 2, which is 4
 ?>
