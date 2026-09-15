44634ea50a648b80e0a601fcc27ba03bf5db48ff
elinasat <elina.satski@tptlive.ee>
2026-09-16 00:37:58 +0300

    Add U.S page and posts and title

--- us.php
+++ us.php
@@ -1,27 +1,27 @@
 <?php
-$title = 'World';
+$title = 'U.S';
 $posts = [
   [
-    'title' => 'Some world title 1',
-    'content' => 'Some world content 1',
+    'title' => 'Some U.S title 1',
+    'content' => 'Some U.S content 1',
     'date' => 'January 1, 2021',
     'author' => 'Pets',
   ],
   [
-    'title' => 'Some world title 2',
-    'content' => 'Some world content 2',
+    'title' => 'Some U.S title 2',
+    'content' => 'Some U.S content 2',
     'date' => 'January 2, 2021',
     'author' => 'Juss',
   ],
   [
-    'title' => 'Some world title 3',
-    'content' => 'Some world content 3',
+    'title' => 'Some U.S title 3',
+    'content' => 'Some U.S content 3',
     'date' => 'January 3, 2021',
     'author' => 'Alex',
   ],
   [
-    'title' => 'Some world title 4',
-    'content' => 'Some world content 4',
+    'title' => 'Some U.S title 4',
+    'content' => 'Some U.S content 4',
     'date' => 'January 4, 2021',
     'author' => 'Manivald',
   ],
@@ -32,9 +32,6 @@ $posts = [
 <?php include __DIR__ . '/partials/header.php'; ?>
 
     <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
-      <?php include __DIR__ . '/partials/featured.php'; ?>
-
       <div class="row g-5">
         <div class="col-md-8">
           <?php include __DIR__ . '/partials/posts.php'; ?>
