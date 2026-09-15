25fec0d28566a4aaf760552577e11c46a8d58c27
elinasat <elina.satski@tptlive.ee>
2026-09-15 22:13:02 +0300

    Add php templating example

--- index.php
+++ index.php
@@ -1,43 +1,27 @@
 <?php
+$name = 'Elina';
+$coinFlip = rand(0,1);
+?>
 
-// libary
-
-class Job {
-    public function task(Logger $logger) {
-        for($i=0;$i<10;$i++){
-            //some work is done (video proccessing or what ever)
-            $logger->log("Task $i was done!");
-        }
-    }
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message) {
-        echo "$message\n";
-    }
-}
-
-class NothingLogger implements Logger {
-    public function log($message) {
-
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-
-// user code
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-$job = new Job();
-$logger = new FileLogger();
-$job->task($logger);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!</h1>
+    <?php if($coinFlip): ?>
+        <h1>Kull</h1>
+    <?php else: ?>
+        <h1>Kiri</h1>
+    <?php endif ?>
+    
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+            <li><?=$i?></li>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
