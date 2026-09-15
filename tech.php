7e05efb5540ef89a615e1d4c35973ab76cdec574
elinasat <elina.satski@tptlive.ee>
2026-09-16 01:18:18 +0300

    Add Tech

--- tech.php
+++ tech.php
@@ -1,27 +1,27 @@
 <?php
-$title = 'World';
+$title = 'Tech';
 $posts = [
   [
-    'title' => 'Some world title 1',
-    'content' => 'Some world content 1',
+    'title' => 'Some Tech title 1',
+    'content' => 'Some Tech content 1',
     'date' => 'January 1, 2021',
     'author' => 'Pets',
   ],
   [
-    'title' => 'Some world title 2',
-    'content' => 'Some world content 2',
+    'title' => 'Some Tech title 2',
+    'content' => 'Some Tech content 2',
     'date' => 'January 2, 2021',
     'author' => 'Juss',
   ],
   [
-    'title' => 'Some world title 3',
-    'content' => 'Some world content 3',
+    'title' => 'Some Tech title 3',
+    'content' => 'Some Tech content 3',
     'date' => 'January 3, 2021',
     'author' => 'Alex',
   ],
   [
-    'title' => 'Some world title 4',
-    'content' => 'Some world content 4',
+    'title' => 'Some Tech title 4',
+    'content' => 'Some Tech content 4',
     'date' => 'January 4, 2021',
     'author' => 'Manivald',
   ],
@@ -30,10 +30,8 @@ $posts = [
 ?>
 
 <?php include __DIR__ . '/partials/header.php'; ?>
-    <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
-      <?php include __DIR__ . '/partials/featured.php'; ?>
 
+    <main class="container">
       <div class="row g-5">
         <div class="col-md-8">
           <?php include __DIR__ . '/partials/posts.php'; ?>
